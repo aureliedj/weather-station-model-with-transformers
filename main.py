1c378b814f9b36200f3d2495d3065b347558a7f6
aureliedj <aurelie.dejong@live.com>
2026-03-06 06:48:48 +0100

    Update

--- main.py
+++
@@ -1,4 +0,0 @@
-from peakweather.dataset import PeakWeatherDataset
-ds = PeakWeatherDataset(root="/Users/aureliedejong/Documents/ETH/_DAS Project/PeakWeatherDataset")
-
-print(ds.get_observations(stations='KLO'))
